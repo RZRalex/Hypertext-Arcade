943ec3fe6cd82238cb0610faaec205faecd360f3
RZRalex <alexxkayne@gmail.com>
2021-05-06 22:03:51 -0700

    world field displays properly

--- pacman.js
+++ pacman.js
@@ -1,14 +1,14 @@
 // Pacman script
 
 var world = [
-    [2,2,2,2,2,2,2,2,2,2,2,2],
-    [2,1,1,1,1,1,1,1,1,1,1,2],
-    [2,1,1,2,2,2,2,2,1,1,1,2],
-    [2,1,1,1,1,1,1,2,1,1,1,2],
-    [2,1,1,1,2,2,2,2,1,1,1,2],
-    [2,1,1,2,2,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,1,1,2],
-    [2,2,2,2,2,2,2,2,2,2,2,2],
+	[1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,2,2,2,1,2,2,2,2,1],
+    [1,2,1,2,2,2,2,1,1,2,1],
+    [1,2,1,1,1,1,2,2,1,2,1],
+    [1,2,2,2,2,1,2,2,2,2,1],
+    [1,2,2,1,1,1,1,1,1,2,1],
+    [1,2,2,2,2,2,2,2,2,2,1],
+    [1,1,1,1,1,1,1,1,1,1,1],
 ];
 
 var worldDict = {
@@ -21,14 +21,27 @@ function drawWorld(){
     output = '';
 
     for(var row = 0; row < world.length; row++){
-        output += '<div class = "row"></div>'
+        output += '<div class = "row">'
         for(var x = 0; x < world[row].length; x++){
-            output += `<div class = ${worldDict[world[row][x]]}></div`
+            output += `<div class = "${worldDict[world[row][x]]}"></div>`
         }
-        output += '</div>'
+        output += "</div>"
     }
     document.getElementById('world').innerHTML = output;
 }
+drawWorld();
+
+var ninjaman = {
+    x: 1,
+    y: 1
+}
+
+function drawNinjaman(){
+    document.getElementById('ninjaman').style.top = ninjaman.y * 40 + 'px'
+    document.getElementById('ninjaman').style.left = ninjaman.x * 40 + 'px'
+    //document.write(score)
+}
+drawNinjaman()
 
 
 var score = 0
